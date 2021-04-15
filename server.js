6b4664d991e772f4970f696453d794986d2cce92
Pastel Turtle <60445211+beckyeng226@users.noreply.github.com>
2021-04-15 15:49:10 -0400

    good through step 4

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
+
+app.get('/api/quotes/random', (req, res) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send({quote: randomQuote});
+});
